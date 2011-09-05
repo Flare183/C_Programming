18c2b4b4d9d5e1168b9cb7d1dc41a511bdc36a1d
Flare183 <flare@flare183.org>
2011-09-05 18:54:38 -0400

    Assignment One done

---
+++ Assignment_Problems/Assignment1/assignment1.c
@@ -0,0 +1,40 @@
+/****************************************************************************
+   assignment1.c      Converts Days into weeks and days
+
+   Inputs:            totalDays - Total Days
+
+
+   Outputs:           Weeks - Total Weeks
+                      Days - Converted Days
+
+   Written by: Jesse N. Richardson     Date: Mon, 05 Sep 2011 18:46:54 -0400
+
+
+  Program Compiles Perfectly
+  Program Runs Perfectly
+
+******************************************************************************/
+
+
+
+#include <stdio.h>
+
+int main (void)
+{
+  int days;
+  int weeks;
+  int totalDays;
+
+
+  printf("Please enter the number of the days\n");
+  scanf("%d", &totalDays);
+  
+  weeks = totalDays / 7;
+
+  days = totalDays % 7;
+
+  printf("Weeks: %d\n", weeks);
+  printf("Days: %d\n", days);
+
+  return 0;
+};
